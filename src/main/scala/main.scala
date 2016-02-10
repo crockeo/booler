e73cd0bec97f1a2eaf332a70aeba8385854bf36e
Cerek Hillen <crockeo@gmail.com>
2016-02-10 17:58:35 -0500

    Updated to be able to parse symmetry from the command line.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,15 +1,27 @@
 package com.github.crockeo.booler
 
 object Main {
+  // "symmetry" program mode.
+  def symmetry(e: Expression): Unit = {
+    println("Checking symmetry...")
+    for (x <- 0.to(Eval.countVars(e)))
+      println("Symmetric at " + x + ": " + (if (Eval.isSymmetric(e, x)) "Y"
+                                            else                        "N"))
+  }
+
   // The entry point to the program.
   def main(args: Array[String]): Unit = {
     if (args.length < 2)
       println("Proper usage: booler.jar <mode> <expression to parse>")
     else {
       val s = args.drop(1).mkString(" ")
+
       Parsing.expression(s) match {
         case None    => println("Failed to parse expression: " + s)
-        case Some(e) => println("no.")
+        case Some(e) => args(0) match {
+          case "symmetry" => symmetry(e)
+          case _          => println("Unrecognized mode "+ args(0) + " .")
+        }
       }
     }
   }
